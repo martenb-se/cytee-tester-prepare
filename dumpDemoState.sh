027e42a51184f547aaae0dc3247025d4ad8395bb
Mårten Björkman <marten.bjorkman@apelkado.com>
2022-05-11 08:54:46 +0200

    Adapt to annoying cp behaviour in 'docker cp' when directory that is being copied to already exists.

--- dumpDemoState.sh
+++ dumpDemoState.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-docker exec -i mongoTest4 /usr/bin/mongodump --username "[Username-Placeholder]" --password "[Password-Placeholder]" --authenticationDatabase admin --db urangutest --out /dump &&\
-docker cp mongoTest4:/dump ./databaseDumps &&\
-mv './databaseDumps/dump' './databaseDumps/urangutest-'"$(date +"%Y-%m-%d_%H-%M-%S")"
-docker exec mongoTest4 rm -fr /dump/urangutest
-
+docker exec -i mongoTest4 /usr/bin/mongodump --username "[Username-Placeholder]" --password "[Password-Placeholder]" --authenticationDatabase admin --db urangutest --out /dump && 
+docker cp mongoTest4:/dump ./tempDump &&
+mkdir ./databaseDumps
+mv ./tempDump/urangutest ./databaseDumps/urangutest-"$(date +"%Y-%m-%d_%H-%M-%S")" && 
+rm -rf ./tempDump &&
+docker exec mongoTest4 rm -fr /dump
