10a6ce26788af84503adf7b1e7fd20fc38aaacad
Mårten Björkman <marten.bjorkman@apelkado.com>
2022-05-11 08:12:39 +0200

    Fixed bug in folder name

--- dumpDemoState.sh
+++ dumpDemoState.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker exec -i mongoTest4 /usr/bin/mongodump --username "[Username-Placeholder]" --password "[Password-Placeholder]" --authenticationDatabase admin --db urangutest --out /dump &&\
+docker exec -i mongoTest4 /usr/bin/mongodump --username "root" --password "root" --authenticationDatabase admin --db urangutest --out /dump &&\
 docker cp mongoTest4:/dump ./databaseDumps &&\
-mv './databaseDumps/urangutest' './databaseDumps/urangutest-'"$(date +"%Y-%m-%d_%H-%M-%S")"
+mv './databaseDumps/dump' './databaseDumps/urangutest-'"$(date +"%Y-%m-%d_%H-%M-%S")"
 docker exec mongoTest4 rm -fr /dump/urangutest
 
